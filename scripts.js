c6a542dd3941d9a1bcf17afb2f0d10df36fb5337
Target52 <gulien@me.com>
2021-09-10 17:57:23 +0200

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,9 @@
+console.log("Hello Winc Academy")
+let name = 'Justin';
+console.log(name)
+let test = 4 + 4;
+console.log(test)
+let testTwee = "4" + "4";
+console.log(testTwee)
+let leeftijd = 48;
+console.log(typeof leeftijd);
